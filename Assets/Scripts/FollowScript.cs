9b6f95b37692c547af67112f33398068740c37e7
edde746 <86283021+edde746@users.noreply.github.com>
2022-03-01 18:23:06 +0100

    Update & Fix Downwards Walking

---
+++ Assets/Scripts/FollowScript.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowScript : MonoBehaviour
+{
+    public Transform target;
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(target.position.x, target.position.y, transform.position.z);
+    }
+}
